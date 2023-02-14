dcd0eb4bd2c7d7761329943221f437c90a628299
ElenaMegia <emegican@myuax.com>
2023-02-14 21:34:38 +0100

    Ejercicio 9

---
+++ src/main/java/org/example/Ejercicio9.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //declarar variables
+        int numeroUsuario;
+        //pedir al usuario que introduzca un número
+        System.out.println("Escriba un múltiplo de 3");
+        numeroUsuario = sc.nextInt();
+        //comprobar si el número introducido es igual al número aleatorio
+        while (numeroUsuario % 3 != 0) {
+            //pedir al usuario que introduzca un número
+            System.out.println("Error " + numeroUsuario + " no es un múltiplo de 3");
+            System.out.println("Escriba un múltiplo de 3");
+            numeroUsuario = sc.nextInt();
+        }
+        //mostrar mensaje de felicitación
+        System.out.println("Ok: " + numeroUsuario + " es un múltiplo de 3");
+    }
+}
