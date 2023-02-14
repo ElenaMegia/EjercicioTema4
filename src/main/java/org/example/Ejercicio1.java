bb3d65beb431aad801f43d3386329dcebb3cc08f
ElenaMegia <emegican@myuax.com>
2023-02-14 20:10:04 +0100

    Ejercicio1

--- src/main/java/org/example/Ejercicio1.java
+++ src/main/java/org/example/Ejercicio1.java
@@ -3,28 +3,30 @@ package org.example;
 import java.util.Scanner;
 
 public class Ejercicio1 {
+//VERSION 2
+    
     static Scanner sc = new Scanner(System.in);
-
-    //Este algoritmo calcula la media de valores (por ejemplo, de las notas), introducidos por el usuario. En primer lugar, el usuario indica el número de valores cuyo promedio desea conocer. Seguidamente, introduce todos estos valores. Finalmente, se muestra la media de estos valores.
-     //crear algoritmo que calcula la media de valores introducidos por el usuario
+// El objetivo sigue siendo realizar el cálculo de la media de un conjunto de valores introducidos por el usuario, pero el usuario ya no tiene que indicar inicialmente el número de valores. Introduce los valores y cuando ha terminado, introduce un valor concreto (-1), para indicar que ha terminado de introducir valores.
     public static void main(String[] args) {
         //declarar variables
-        int numeroValores;
         int valor;
         int suma = 0;
         int media;
-        //pedir al usuario que introduzca el número de valores
-        System.out.println("Introduzca el número de valores: ");
-        numeroValores = sc.nextInt();
+        int numeroValores = 0;
         //pedir al usuario que introduzca los valores
-        for (int i = 1; i <= numeroValores; i++) {
-            System.out.println("Introduzca el valor " + i + ": ");
+        do {
+            System.out.println("Introduzca el valor " + (numeroValores + 1) + ": ");
             valor = sc.nextInt();
-            suma = suma + valor;
-        }
+            if (valor != -1) {
+                suma = suma + valor;
+                numeroValores++;
+            }
+        } while (valor != -1);
         //calcular la media
         media = suma / numeroValores;
         //mostrar la media
         System.out.println("La media de los valores introducidos es: " + media);
     }
+
+
 }
